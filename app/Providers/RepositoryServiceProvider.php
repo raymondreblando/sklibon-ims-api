c27764c3da15b7bb76a4a372dbcc930c81b24d0d
Raymond <raymond.republica.reblando@gmail.com>
2025-08-17 17:47:14 +0800

    added hotline repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,12 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\EloquentHotlineRepository;
 use App\Repositories\Eloquent\EloquentLocationRepository;
 use App\Repositories\Eloquent\EloquentPositionRepository;
 use App\Repositories\Eloquent\EloquentRefreshTokenRepository;
 use App\Repositories\Eloquent\EloquentRoleRepository;
 use App\Repositories\Eloquent\EloquentUserInfoRepository;
 use App\Repositories\Eloquent\EloquentUserRepository;
+use App\Repositories\HotlineRepository;
 use App\Repositories\LocationRepository;
 use App\Repositories\PositionRepository;
 use App\Repositories\RefreshTokenRepository;
@@ -24,6 +26,7 @@ class RepositoryServiceProvider extends ServiceProvider
         UserRepository::class => EloquentUserRepository::class,
         UserInfoRepository::class => EloquentUserInfoRepository::class,
         PositionRepository::class => EloquentPositionRepository::class,
+        HotlineRepository::class => EloquentHotlineRepository::class,
         LocationRepository::class => EloquentLocationRepository::class,
     ];
 
