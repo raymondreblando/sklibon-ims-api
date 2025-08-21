abb0b5cf27a0b78e0317c2a2d5f21a3be69764ef
Raymond <raymond.republica.reblando@gmail.com>
2025-08-21 09:21:03 +0800

    added request type model and migration

---
+++ database/migrations/2025_08_21_091720_create_request_types_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request_types', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('name', 150)->unique();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request_types');
+    }
+};
