80828e9805767d0cd3cc98d060b46fd2069f7ae2
Raymond <raymond.republica.reblando@gmail.com>
2025-08-22 13:43:11 +0800

    define report, barangay, user and attachment relations

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attachment extends Model
@@ -23,4 +24,9 @@ class Attachment extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function report(): BelongsTo
+    {
+        return $this->belongsTo(Report::class, 'report_id', 'id');
+    }
 }
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Report extends Model
@@ -25,4 +27,19 @@ class Report extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function barangay(): BelongsTo
+    {
+        return $this->belongsTo(Barangay::class, 'barangay_id', 'id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasMany(Attachment::class, 'report_id', 'id');
+    }
 }
