383456a273cbf51c5a69e5ce95199a719534195c
Raymond <raymond.republica.reblando@gmail.com>
2025-09-05 22:01:42 +0800

    fix additonal address

--- app/Http/Requests/V1/Account/UpdateProfileRequest.php
+++ app/Http/Requests/V1/Account/UpdateProfileRequest.php
@@ -37,7 +37,7 @@ class UpdateProfileRequest extends FormRequest
                 'max:100',
                 Rule::unique('users', 'email')->ignore($this->user()->id)
             ],
-            'info' => ['array:firstname,middlename,lastname,gender,age,phone_number,birthdate,position_id,province_id,municipality_id,barangay_id,additional_address'],
+            'info' => ['array:firstname,middlename,lastname,gender,age,phone_number,birthdate,position_id,province_id,municipality_id,barangay_id,addtional_address'],
             'info.firstname' => ['required', 'string', 'max:100'],
             'info.middlename' => ['nullable', 'string', 'max:100'],
             'info.lastname' => ['required', 'string', 'max:100'],
@@ -54,7 +54,7 @@ class UpdateProfileRequest extends FormRequest
             'info.province_id' => ['required', 'string', 'exists:provinces,id'],
             'info.municipality_id' => ['required', 'string', 'exists:municipalities,id'],
             'info.barangay_id' => ['required', 'string', 'exists:barangays,id'],
-            'info.additional_address' => ['nullable', 'string', 'max:255'],
+            'info.addtional_address' => ['nullable', 'string', 'max:255'],
         ];
     }
 }
